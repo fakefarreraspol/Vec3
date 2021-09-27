451604feda9b87655f7cec521c6c31fc473a9ae9
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2021-09-27 19:00:32 +0200

    Vec3

---
+++ Vectors/Vectors/Header.h
@@ -0,0 +1,107 @@
+#ifndef __VECTOR_3__
+#define __VECTOR_3__
+
+#include <math.h>
+
+template<class TYPE>
+
+class Vec3
+{
+protected:
+	TYPE x, y, z;
+public: 
+	
+
+	Vec3(){}
+
+	Vec3(TYPE& x, TYPE& y, TYPE& z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+
+	Vec3(const Vec3<TYPE>& vec)
+	{
+		this->x = vec.x;
+		this->y = vec.y;
+		this->z = vec.z;
+		return *this;
+	}
+	//operator
+	Vec3 operator +(const Vec3& vec) const
+	{
+		result r;
+
+		r.x = x + vec.x;
+		r.y = y + vec.y;
+		r.z = z + vec.z;
+
+		return r;
+	}
+	Vec3 operator -(const Vec3& vec) const
+	{
+		result r;
+
+		r.x = x - vec.x;
+		r.y = y - vec.y;
+		r.z = z - vec.z;
+
+		return r;
+	}
+
+	Vec3& operator -=(const Vec3& vec)
+	{
+		this->x -= vec.x;
+		this->y -= vec.y;
+		this->z -= vec.z;
+
+		return(*this);
+
+	}
+
+	Vec3& operator +=(const Vec3& vec)
+	{
+		this->x += vec.x;
+		this->y += vec.y;
+		this->z += vec.z;
+
+		return(*this);
+
+	}
+	bool operator ==(const Vec3& vec) const
+	{
+		return (x == vec.x && y == vec.y && z==vec.z);
+	}
+	bool IsZero() const
+	{
+		return (x == 0 && y == 0 && z == 0);
+	}
+
+	Vec3& SetToZero()
+	{
+		x = y = z = 0;
+		return(*this);
+	}
+
+	Vec3& Normalize()
+	{
+		x = y = z = 1;
+		return(*this);
+	}
+
+	TYPE DistanceTo(const Vec3& vec) const
+	{
+		TYPE fx = x - vec.x;
+		TYPE fy = y - vec.y;
+		TYPE fz = z - vec.z;
+		return sqrtf((fx * fx) + (fy * fy) + (fz * fz));
+	}
+};
+
+
+
+
+
+
+#endif __VECTOR_3__
