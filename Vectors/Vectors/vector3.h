c63a522017b29bf20fe382e53afb5b91e69bdb14
fakefarreraspol <79444349+fakefarreraspol@users.noreply.github.com>
2021-09-27 22:31:42 +0200

    vec3
    vec3

--- Vectors/Vectors/vector3.h
+++ Vectors/Vectors/vector3.h
@@ -74,9 +74,9 @@ public:
 	}
 	Vec3& operator ==(const Vec3& vec) 
 	{
-		this->x == vec.x;
-		this->y == vec.y;
-		this->z == vec.z;
+		this->x = vec.x;
+		this->y = vec.y;
+		this->z = vec.z;
 		return (*this);
 	}
 	bool IsZero() const
